56d6cc980a99de37a07281e638bc9de1995c788d
Maksim Zinal <maxzinal@yandex.ru>
2023-09-07 13:30:58 +0300

    дополнительные примеры

--- ddb-maint-config.sh
+++ ddb-maint-config.sh
@@ -5,13 +5,13 @@
 yc_profile=`yc config profile list | grep -E 'ACTIVE$' | (read x y && echo $x)`
 yc_folder=`yc config profile get ${yc_profile} | grep -E '^folder-id: ' | (read x y && echo $y)`
 # Service account and cloud function names
-sa_name=delta-sa1
+sa_name=delta-maint-sa1
 cf_ddb_name=delta-ddb-cleanup
 cf_s3_name=delta-s3-cleanup
 # YDB DocAPI endpoint and table name
-docapi_endpoint=https://docapi.serverless.yandexcloud.net/ru-central1/b1gfvslmokutuvt2g019/etngt3b6eh9qfc80vt54
+docapi_endpoint=https://docapi.serverless.yandexcloud.net/ru-central1/b1gfvslmokutuvt2g019/etnac7v7lqqiflor0sem
 docapi_table=delta_log
 # S3 file with the list of prefixes to be cleaned up
-s3_prefix_file=s3://dproc-wh/config/delta-prefixes.txt
+s3_prefix_file=s3://mzinal-wh1/delta-prefixes.txt
 
 # End Of File
